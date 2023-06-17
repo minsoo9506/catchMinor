30e3d671f8f2878f1ec1e06dfc76d605d25edeb6
kakao <kakao@kakaoui-MacBookPro-2.local>
2023-06-17 16:09:33 +0900

    feat(dataset.py): make window-based dataset class

--- catchMinor/data_load/dataset.py
+++ catchMinor/data_load/dataset.py
@@ -24,30 +24,26 @@ class tabularDataset(Dataset):
         return self.x[idx, :], self.y[idx, :]
 
 
-# TODO: univariate랑 multivariate 구분없이 사용할 수 있도록 아래 코드 수정
-# univariate일 때, 그냥 dummy axis 하나 더 있는거지!
+class WindowDataset(Dataset):
+    """Window-based dataset in univariate & multivariate time series data.
 
+    if x.dim is 1, then feature_dim will be 1.
+    |self.x| = (data_len, window_size, feature_dim)
+    |self.y| = (data_len, window_size, feature_dim)
+    """
 
-class UnivariateBaseWindowDataset(Dataset):
-    """Window based dataset in univariate time series data"""
-
-    def __init__(self, x: np.ndarray, y: np.ndarray, window_size: int):
+    def __init__(self, x: np.ndarray, y: np.ndarray, window_size: int, overlaps: bool):
         """
         Args:
             x (np.ndarray): input feature
             y (np.ndarray): input label
             window_size (int): window size of input
+            overlaps (bool) : true, if want to make overlap dataset
         """
-
         super().__init__()
 
-        data_len = len(x) - window_size + 1
-        self.x = np.zeros((data_len, window_size))
-        self.y = np.zeros((data_len, window_size))
-
-        for idx in range(data_len):
-            self.x[idx, :] = x[idx : idx + window_size]
-            self.y[idx, :] = y[idx : idx + window_size]
+        self.x = self._make_window_based_data(x, window_size, overlaps)
+        self.y = self._make_window_based_data(y, window_size, overlaps)
 
         self.x = torch.tensor(self.x, dtype=torch.float32)
         self.y = torch.tensor(self.y, dtype=torch.float32)
@@ -57,44 +53,87 @@ class UnivariateBaseWindowDataset(Dataset):
 
     def __getitem__(self, idx: int) -> tuple[torch.tensor, torch.tensor]:
         return self.x[idx, :], self.y[idx, :]
+
+    def _make_window_based_data(
+        self, data: np.ndarray, window_size: int, overlaps: int
+    ) -> np.ndarray:
+        if data.ndim == 1:
+            data = np.expand_dims(data, axis=-1)
+        if overlaps:
+            _data = self._overlaps(data, window_size)
+        else:
+            _data = self._non_overlaps(data, window_size)
+        return _data
+
+    def _overlaps(self, data: np.ndarray, window_size: int) -> np.ndarray:
+        data_len = len(data) - window_size + 1
+        feature_dim = data.shape[-1]
+        _data = np.zeros((data_len, window_size, feature_dim))
+        for idx in range(data_len):
+            _data[idx, :] = data[idx : idx + window_size]
+        return _data
+
+    def _non_overlaps(self, data: np.ndarray, window_size: int) -> np.ndarray:
+        data_len = len(data) // window_size
+        feature_dim = data.shape[-1]
+        _data = np.zeros((data_len, window_size, feature_dim))
+        for idx in range(data_len):
+            _data[idx, :] = data[idx * window_size : (idx + 1) * window_size]
+        return _data
 
 
 # LSTM input = (N,L,H) when batch_first=True
 # = (batch_size, seq_length, hidden_size)
-class UnivariateLSTMWindowDataset(Dataset):
-    def __init__(self, x: np.ndarray, y: np.ndarray, window_size: int):
-        super().__init__()
+# class UnivariateLSTMWindowDataset(Dataset):
+#     def __init__(self, x: np.ndarray, y: np.ndarray, window_size: int):
+#         super().__init__()
 
-        data_len = len(x) - window_size + 1
-        self.x = np.zeros((data_len, window_size))
-        self.y = np.zeros((data_len, window_size))
+#         data_len = len(x) - window_size + 1
+#         self.x = np.zeros((data_len, window_size))
+#         self.y = np.zeros((data_len, window_size))
 
-        for idx in range(data_len):
-            self.x[idx, :] = x[idx : idx + window_size]
-            self.y[idx, :] = y[idx : idx + window_size]
+#         for idx in range(data_len):
+#             self.x[idx, :] = x[idx : idx + window_size]
+#             self.y[idx, :] = y[idx : idx + window_size]
 
-        # add axis (hidden_size)
-        self.x = self.x[:, :, np.newaxis]
-        self.y = self.y[:, :, np.newaxis]
+#         # add axis (hidden_size)
+#         self.x = self.x[:, :, np.newaxis]
+#         self.y = self.y[:, :, np.newaxis]
 
-        self.x = torch.tensor(self.x, dtype=torch.float32)
-        self.y = torch.tensor(self.y, dtype=torch.float32)
+#         self.x = torch.tensor(self.x, dtype=torch.float32)
+#         self.y = torch.tensor(self.y, dtype=torch.float32)
 
-    def __len__(self) -> int:
-        return self.x.shape[0]
+#     def __len__(self) -> int:
+#         return self.x.shape[0]
 
-    def __getitem__(self, idx: int) -> tuple[torch.tensor, torch.tensor]:
-        return self.x[idx, :], self.y[idx, :]
+#     def __getitem__(self, idx: int) -> tuple[torch.tensor, torch.tensor]:
+#         return self.x[idx, :], self.y[idx, :]
 
 
 if __name__ == "__main__":
-    # univariate time series data -> window-based approach
-    dataset = UnivariateLSTMWindowDataset(
+    # univariate time series data
+    uni_dataset = WindowDataset(
         x=np.array([1, 2, 3, 4, 5, 6, 7, 8, 9]),
-        y=np.array([1, 2, 3, 4, 5, 6, 7, 8, 9]),
-        window_size=3,
+        y=np.array([1, 0, 0, 0, 1, 0, 0, 1, 0]),
+        window_size=2,
+        overlaps=True
     )
-    data_loader = DataLoader(dataset, batch_size=2)
+    data_loader = DataLoader(uni_dataset, batch_size=2)
+    print(">> univariate time series dataset")
+    for x, _ in data_loader:
+        print(x.shape)
+        print(x)
+        break
+
+    # multivariate time series data
+    multi_dataset = WindowDataset(
+        x=np.array([[1,2,3,4,5],[5,4,3,2,1],[1,2,3,4,5]]),
+        y=np.array([1, 0, 0, 0, 1, 0, 0, 1, 0]),
+        window_size=2,
+        overlaps=True
+    )
+    data_loader = DataLoader(multi_dataset, batch_size=2)
+    print(">> multivariate time series dataset")
     for x, _ in data_loader:
         print(x.shape)
         print(x)
