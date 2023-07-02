ebb1cef8cb1287b542e0b997b5728379f553ee62
kakao <kakao@kakaoui-MacBookPro-2.local>
2023-07-02 15:19:55 +0900

    feat(dataset.WindowDataset): add choose shape arg (window or feature_dim can be first)

--- catchMinor/data_load/dataset.py
+++ catchMinor/data_load/dataset.py
@@ -32,18 +32,28 @@ class WindowDataset(Dataset):
     |self.y| = (data_len, window_size, feature_dim)
     """
 
-    def __init__(self, x: np.ndarray, y: np.ndarray, window_size: int, overlaps: bool):
+    def __init__(
+        self,
+        x: np.ndarray,
+        y: np.ndarray,
+        window_size: int,
+        overlaps: bool,
+        shape: str = "wf",
+    ):
         """
         Args:
             x (np.ndarray): input feature
             y (np.ndarray): input label
             window_size (int): window size of input
             overlaps (bool) : true, if want to make overlap dataset
+            shape (str) : wf is (window_size, feature_dim), fw is (feature_dim, window_size)
         """
         super().__init__()
 
-        self.x = self._make_window_based_data(x, window_size, overlaps)
-        self.y = self._make_window_based_data(y, window_size, overlaps)
+        assert shape in ["wf", "fw"], "shape has to be wf or fw"
+
+        self.x = self._make_window_based_data(x, window_size, overlaps, shape)
+        self.y = self._make_window_based_data(y, window_size, overlaps, shape)
 
         self.x = torch.tensor(self.x, dtype=torch.float32)
         self.y = torch.tensor(self.y, dtype=torch.float32)
@@ -55,30 +65,42 @@ class WindowDataset(Dataset):
         return self.x[idx, :], self.y[idx, :]
 
     def _make_window_based_data(
-        self, data: np.ndarray, window_size: int, overlaps: int
+        self, data: np.ndarray, window_size: int, overlaps: int, shape: str
     ) -> np.ndarray:
         if data.ndim == 1:
             data = np.expand_dims(data, axis=-1)
         if overlaps:
-            _data = self._overlaps(data, window_size)
+            _data = self._overlaps(data, window_size, shape)
         else:
-            _data = self._non_overlaps(data, window_size)
+            _data = self._non_overlaps(data, window_size, shape)
         return _data
 
-    def _overlaps(self, data: np.ndarray, window_size: int) -> np.ndarray:
+    def _overlaps(self, data: np.ndarray, window_size: int, shape: str) -> np.ndarray:
         data_len = len(data) - window_size + 1
         feature_dim = data.shape[-1]
-        _data = np.zeros((data_len, window_size, feature_dim))
-        for idx in range(data_len):
-            _data[idx, :] = data[idx : idx + window_size]
+        if shape == "wf":
+            _data = np.zeros((data_len, window_size, feature_dim))
+            for idx in range(data_len):
+                _data[idx, :] = data[idx : idx + window_size]
+        else:
+            _data = np.zeros((data_len, feature_dim, window_size))
+            for idx in range(data_len):
+                _data[idx, :] = data[idx : idx + window_size].T
         return _data
 
-    def _non_overlaps(self, data: np.ndarray, window_size: int) -> np.ndarray:
+    def _non_overlaps(
+        self, data: np.ndarray, window_size: int, shape: str
+    ) -> np.ndarray:
         data_len = len(data) // window_size
         feature_dim = data.shape[-1]
-        _data = np.zeros((data_len, window_size, feature_dim))
-        for idx in range(data_len):
-            _data[idx, :] = data[idx * window_size : (idx + 1) * window_size]
+        if shape == "wf":
+            _data = np.zeros((data_len, window_size, feature_dim))
+            for idx in range(data_len):
+                _data[idx, :] = data[idx * window_size : (idx + 1) * window_size]
+        else:
+            _data = np.zeros((data_len, feature_dim, window_size))
+            for idx in range(data_len):
+                _data[idx, :] = data[idx * window_size : (idx + 1) * window_size].T
         return _data
 
 
@@ -116,7 +138,8 @@ if __name__ == "__main__":
         x=np.array([1, 2, 3, 4, 5, 6, 7, 8, 9]),
         y=np.array([1, 0, 0, 0, 1, 0, 0, 1, 0]),
         window_size=2,
-        overlaps=True
+        overlaps=True,
+        shape="wf",
     )
     data_loader = DataLoader(uni_dataset, batch_size=2)
     print(">> univariate time series dataset")
@@ -127,10 +150,11 @@ if __name__ == "__main__":
 
     # multivariate time series data
     multi_dataset = WindowDataset(
-        x=np.array([[1,2,3,4,5],[5,4,3,2,1],[1,2,3,4,5]]),
+        x=np.array([[1, 2, 3, 4, 5], [5, 4, 3, 2, 1], [1, 2, 3, 4, 5]]),
         y=np.array([1, 0, 0, 0, 1, 0, 0, 1, 0]),
         window_size=2,
-        overlaps=True
+        overlaps=True,
+        shape="wf",
     )
     data_loader = DataLoader(multi_dataset, batch_size=2)
     print(">> multivariate time series dataset")
