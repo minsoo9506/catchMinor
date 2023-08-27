0caf640275e5c75e33b831e8a1cfd6cbca0767b3
minsoo9506 <ghktjd15gh@gmail.com>
2023-08-27 22:10:09 +0900

    feat(DLinear): DLinear conig

---
+++ catchMinor/time_series_model/DLinear/DLinear_config.py
@@ -0,0 +1,17 @@
+from typing import Optional
+from catchMinor.base.base_config import loss_func_config, model_config, optimizer_config
+
+
+class DLinear_config(model_config):
+    features_dim_list: list[int] = [16, 8, 4, 2]
+    kernel_size: int = 25
+    seq_len: int = 128
+
+
+class DLinear_optimizer_config(optimizer_config):
+    optimizer: str = "Adam"
+    optimizer_params: Optional[dict] = {"lr": 0.0001}
+
+
+class DLinear_loss_func_config(loss_func_config):
+    loss_fn: str = "MSELoss"
